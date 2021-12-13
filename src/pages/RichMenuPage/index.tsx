e8bc7861446dbd122d1b4848c7f84174e1970701
ak1103dev <ak.apichan1103@gmail.com>
2021-12-13 09:36:03 +0700

    :construction: test

--- src/pages/RichMenuPage/index.tsx
+++ src/pages/RichMenuPage/index.tsx
@@ -1,7 +1,27 @@
+import { useState } from 'react'
+import ImageMapper, { CustomArea, AreaEvent } from 'react-img-mapper'
+import Areas from './areas.json'
+
 const RichMenuPage = () => {
+  const [area, setArea] = useState<CustomArea | null>(null)
+  const URL = 'https://raw.githubusercontent.com/img-mapper/react-docs/master/src/assets/example.jpg';
+  const MAP = {
+    name: 'my-map',
+    areas: Areas,
+  }
+
+  const handleClick = (area: CustomArea, index: number, event: AreaEvent) => {
+    console.log(area, index, event)
+    setArea(area)
+  }
+
   return (
     <div>
-      jkljljkl
+      <ImageMapper src={URL} map={MAP} active={false} onClick={handleClick} />
+      {
+        area &&
+        <div>{JSON.stringify(area, null, 2)}</div>
+      }
     </div>
   )
 }
